4964e4931e2bfdc9de82a0fac314ef6fe0f9a5aa
tjruesch <tomj.ruesch@gmail.com>
2021-10-11 13:26:04 +0200

    adjust client for pydantic repr

--- memsource/api_rest/client.py
+++ memsource/api_rest/client.py
@@ -1,16 +1,18 @@
 from typing import List
-from memsource import models, api_rest
+from memsource import api_rest
+
+from memsource.schemas.client import Client as ClientModel
 
 
 class Client(api_rest.BaseApi):
     # Document: https://cloud.memsource.com/web/docs/api#tag/Client
 
-    def create(self, name: str) -> str:
-        return self._post("v1/clients", {"name": name})["id"]
+    def create(self, name: str) -> ClientModel:
+        return ClientModel(**self._post("v1/clients", {"name": name}))
 
-    def get(self, clientID: int) -> models.Client:
-        return models.Client(self._get(f"v1/clients/{clientID}"))
+    def get(self, clientID: int) -> ClientModel:
+        return ClientModel(**self._get(f"v1/clients/{clientID}"))
 
-    def list(self, page: int=0) -> List[models.Client]:
+    def list(self, page: int=0) -> List[ClientModel]:
         clients = self._get("v1/clients", {"page": page})
-        return [models.Client(client) for client in clients.get("content", [])]
+        return [ClientModel(**client) for client in clients.get("content", [])]
