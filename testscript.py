5d111f8b44eb55b4834192940d4460501bf3808c
tjruesch <tomj.ruesch@gmail.com>
2021-10-11 14:02:05 +0200

    add schema representations

--- testscript.py
+++ testscript.py
@@ -5,4 +5,10 @@ pp = PrettyPrinter()
 
 m = Memsource(user_name='tj.ruesch', password='#b4cyYCz#hVXhT*')
 
-m.client.create("Test Client")
+# test client
+customer = m.client.create("Test Client 7")
+customer_list = m.client.list()
+m.client.get(customer_list[0])
+
+project = m.project.list()[0]
+pp.pprint(project)
